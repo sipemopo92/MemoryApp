569fe6bc1a5c02cdee24d6bcdfaeee1b7026d602
sipemopo92 <sipemopo92@gmail.com>
2023-02-28 19:10:02 +0100

    init

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'main'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
