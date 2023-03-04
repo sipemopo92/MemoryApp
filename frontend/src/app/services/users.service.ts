52ec5384e763fc7592918a5df1da4cc20dd46519
sipemopo92 <sipemopo92@gmail.com>
2023-03-04 18:37:12 +0100

    minor update css

--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -9,7 +9,7 @@ import { User } from '../models/user';
 export class UsersService {
 
   private apiUrl = 'http://localhost:3000/users'
-  private test = true;
+  private test = false;
   private user: User | null = null;
   private userLogged: boolean = false;
 
